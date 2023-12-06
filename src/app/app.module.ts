38666077988f1fcc621791fc8a767af3e4837d27
VictorMmontanari <victormm9000@gmail.com>
2023-12-06 14:52:48 -0300

    router create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';  // Add this import
+import { AppRoutingModule } from './app.routes';  // Add this import
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { LoaderComponent } from './loader/loader.component';
-import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,8 +20,10 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule, 
-    [FormsModule],// Adicione esta linha
+    HttpClientModule,
+    FormsModule,
+    RouterModule,  // Add this line
+    AppRoutingModule,  // Add this line
     // outros módulos necessários
   ],
   providers: [],
---
+++ src/app/fakeapi/fakeapi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fakeapi',
+  templateUrl: './fakeapi.component.html',
+  styleUrl: './fakeapi.component.css'
+})
+export class FakeapiComponent {
+
+}
